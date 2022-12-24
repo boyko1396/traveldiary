c4e257098f637b25da89cdd5a8bd561605018910
Vasya <boyko1396@gmail.com>
2022-12-24 17:37:20 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
-// link-moore
-var linkMoore = document.querySelector('.js-link-moore');
-var mooreContent = document.querySelector('.project-about__text');
 
-linkMoore.addEventListener('click', function() {
-  linkMoore.classList.toggle('is-hide');
-  mooreContent.classList.add('is-show');
+$(document).ready(function() {
+  // scroll-up btn show
+  var targetItem = $('.intro__scroll-up');
+  var scrollShow = 600;
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= scrollShow) {
+      targetItem.removeClass('is-hidden');
+    } else {
+      targetItem.addClass('is-hidden');
+    }
+  });
+
+  // scroll target
+  $('.js-scroll-target').on('click', function (event){
+    event.preventDefault();
+    var scrollId = $(this).attr('href');
+    var scrollHeightTop = $(scrollId).offset().top;
+    $('body, html').animate({scrollTop: scrollHeightTop}, 650);
+  });
+
+  // scroll target
+  $('.js-link-moore').on('click', function (event){
+    event.preventDefault();
+    $(this).toggleClass('is-active');
+    $('.project-about__text-moore').slideToggle(450);
+  });
 });
 
 // header nav mobile
@@ -16,27 +38,6 @@ btnNav.addEventListener('click', function() {
   bodyNav.classList.toggle('is-menu-opened');
   btnNav.classList.toggle('is-active');
   headerDropdown.classList.toggle('is-show');
-});
-
-// scroll-up btn show
-var targetItem = $('.intro__scroll-up');
-var scrollShow = 600;
-$(window).scroll(function() {
-  var scroll = $(window).scrollTop();
-
-  if (scroll >= scrollShow) {
-    targetItem.removeClass('is-hidden');
-  } else {
-    targetItem.addClass('is-hidden');
-  }
-});
-
-// scroll target
-$('.js-scroll-target').on('click', function (event){
-  event.preventDefault();
-  var scrollId = $(this).attr('href');
-  var scrollHeightTop = $(scrollId).offset().top;
-  $('body, html').animate({scrollTop: scrollHeightTop}, 650);
 });
 
 // project card
